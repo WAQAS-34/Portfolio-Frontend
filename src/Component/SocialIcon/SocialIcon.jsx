a4e12a61b35b32348a5937fedd6bf5dd509ec2bf
uzma-nazim <uuzzi0216@gmail.com>
2024-12-06 00:39:02 +0500

    change done

---
+++ src/Component/SocialIcon/SocialIcon.jsx
@@ -0,0 +1,43 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+const SocialIcon = () => {
+  const router = useRouter();
+  const handleDirect = (url) => {
+    window.open(url, "_blank");
+  };
+  return (
+    <div
+      style={{
+        backgroundColor: "#000",
+        position: "fixed",
+        bottom: "15px",
+        right: "15px",
+        padding: "10px",
+        borderRadius: "10px",
+        cursor: "pointer",
+        zIndex: 999,
+      }}
+    >
+      <img
+        className="social-icon"
+        src="/images/fiverr.png"
+        onClick={() =>
+          handleDirect(
+            "https://www.upwork.com/fl/~01d7f38bba1c799002?mp_source=share"
+          )
+        }
+        style={{ width: "60px", marginRight: "5px" }}
+      />
+      <img
+        className="social-icon"
+
+        onClick={() => handleDirect("https://www.fiverr.com/s/EgPZDb9")}
+        src="/images/upwok.png"
+        style={{ width: "60px" }}
+      />
+    </div>
+  );
+};
+
+export default SocialIcon;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,39 @@
-import ContactForm from '@/Component/ContactForm/ContactForm'
-import Experties from '@/Component/Experties/Experties'
-import Header from '@/Component/Header/Header.jsx'
-import HeroSection from '@/Component/HeroSection/HeroSection'
-import Portfolio from '@/Component/Portfolio/Portfolio'
-import Services from '@/Component/Services/Services'
-import ServicesSlides from '@/Component/ServicesSlides/ServicesSlides'
-import SkillsTab from '@/Component/SkillsTab/SkillsTab'
-import { Element } from 'react-scroll'
-
+import ContactForm from "@/Component/ContactForm/ContactForm";
+import Experties from "@/Component/Experties/Experties";
+import Header from "@/Component/Header/Header.jsx";
+import HeroSection from "@/Component/HeroSection/HeroSection";
+import Portfolio from "@/Component/Portfolio/Portfolio";
+import Services from "@/Component/Services/Services";
+import ServicesSlides from "@/Component/ServicesSlides/ServicesSlides";
+import SkillsTab from "@/Component/SkillsTab/SkillsTab";
+import SocialIcon from "@/Component/SocialIcon/SocialIcon";
+import WhatsAppIcon from "@/Component/WhatsUpFloatingIcon/WhatsUpFloatingIcon";
+import { Element } from "react-scroll";
 
 export default function Home() {
   return (
     <>
-
+      <WhatsAppIcon />
+      <SocialIcon />
       <Header />
-      <Element name='home'>
+      <Element name="home">
         <HeroSection />
       </Element>
 
       {/* <ServicesSlides /> */}
 
-      <Element name="services" id='services'>
+      <Element name="services" id="services">
         <Services />
       </Element>
-      <Element name="portfolio" id='portfolio'>
-
+      <Element name="portfolio" id="portfolio">
         <Portfolio />
       </Element>
-      <Element name="Expertise" id='Expertise'>
-
+      <Element name="Expertise" id="Expertise">
         <Experties />
       </Element>
-      <Element name="contact" id='contact'>
-
+      <Element name="contact" id="contact">
         <ContactForm />
       </Element>
-
-
-
     </>
-  )
+  );
 }
